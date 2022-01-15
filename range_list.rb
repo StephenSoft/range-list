7461934f28162101da392561962bf359df910a9b
StephenZzzz <stephenzhao94@163.com>
2022-01-16 00:40:24 +0800

    add private get set & add print login & add (add) logic

--- range_list.rb
+++ range_list.rb
@@ -1,24 +1,49 @@
 require 'byebug'
 require './validate/validate_add_params'
 class RangeList
-  attr_accessor :implement
+
+  attr_reader :implement, :current_implement_hash
 
   def initialize(implement = [])
     @implement = implement
+    @current_implement_hash = {}
   end
 
+  # key_* 0, 1, 2, 3, ++ ....
   def add(range)
     ::Validate::ValidateAddParams.new(range).execute
-
-  # TODO
+    if current_implement_hash.empty?
+      @current_implement_hash = {key_0: {come: range.first, to: range.last}}
+    else
+      compara_and_build_new(range)
+    end
   end
 
   def remove
-  #TODO
+    # TODO: Add remove logic
   end
 
   def print
-    # TODO
+    p format_range_lists
+  end
+
+  private
+
+  attr_writer :implement, :current_implement_hash
+
+  def compara_and_build_new(range)
+    # TODO: Add compara logic
+
+  end
+
+  def format_range_lists
+    range_lists_string = ""
+
+    current_implement_hash.each_value do |value|
+      range_lists_string << "[ #{value[:come]}, #{value[:to]} ) "
+    end
+
+    range_lists_string
   end
 end
 
