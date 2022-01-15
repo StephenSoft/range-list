88d73c42bdfaf829145bcbe764052f9fa46fb048
StephenZzzz <stephenzhao94@163.com>
2022-01-15 22:10:56 +0800

    clean code

--- validate/validate_add_params.rb
+++ validate/validate_add_params.rb
@@ -10,18 +10,24 @@ module  Validate
     end
 
     def execute
-      validate_type(params)
-      validate_length(params.size)
+      validate_type
+      validate_length
+      validate_range
     end
 
     private
-
-    def validate_length(length)
-      raise RangeListParamsError, "Size error params size only <= 2" if length <= 1 || length >= 3
+    def validate_type
+      raise RangeListParamsError, "Type error This method need Array params" unless params.is_a?(Array)
     end
 
-    def validate_type(params)
-      raise RangeListParamsError, "Type error This method need Array params" unless params.is_a?(Array)
+    def validate_length
+      raise RangeListParamsError, "Size error params size only <= 2" if params.size <= 1 || params.size >= 3
+    end
+
+    def validate_range
+      if params.last - params.first <= 0
+        raise RangeListParamsError, "Range error need to: value > come: value, likie [3, 10], [5,10]"
+      end
     end
 
   end
